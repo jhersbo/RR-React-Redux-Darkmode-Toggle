c8f7ebee473a0dc7804330c23d95c07c59c1b184
jhersbo <jhersbo@gmail.com>
2022-01-30 17:52:59 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
-import { useSelector, useDispatch, connect } from 'react-redux'
-import { clearData, fetchData, incrementId, decrementId, inputId } from './features/dataSlice'
-import { useEffect } from 'react';
+
+import { 
+  useSelector, 
+  useDispatch, 
+  connect 
+} from 'react-redux'
+
+import { 
+  clearData, 
+  fetchData, 
+  incrementId, 
+  decrementId, 
+  inputId 
+} from './features/dataSlice'
+
+import { 
+  useEffect 
+} from 'react';
 
 function App(props) {
   const dispatch = useDispatch()
